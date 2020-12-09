1e2537b22a0ceef9f4cbdbcbf9b2a35f80ed6b61
molpopgen <krthornt@uci.edu>
2020-12-09 10:25:52 -0800

    Silence warnings from tests.

--- src/test_table_collection.rs
+++ src/test_table_collection.rs
@@ -7,8 +7,8 @@ mod tests {
     fn test_add_edge_table_rows() -> () {
         let mut edges: MaybeUninit<tsk_edge_table_t> = MaybeUninit::uninit();
         unsafe {
-            let mut rv = tsk_edge_table_init(edges.as_mut_ptr(), 0);
-            rv = tsk_edge_table_add_row(edges.as_mut_ptr(), 0., 10., 0, 1, std::ptr::null(), 0);
+            let _ = tsk_edge_table_init(edges.as_mut_ptr(), 0);
+            let _ = tsk_edge_table_add_row(edges.as_mut_ptr(), 0., 10., 0, 1, std::ptr::null(), 0);
             assert_eq!((*edges.as_ptr()).num_rows, 1);
             tsk_edge_table_free(edges.as_mut_ptr());
         }
