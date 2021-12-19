451c62a3dec17de775d3193d7c5e598b9b6a3a3f
molpopgen <krthornt@uci.edu>
2021-12-19 15:49:49 -0800

    Add test block to example.

--- examples/tree_traversals.rs
+++ examples/tree_traversals.rs
@@ -47,3 +47,90 @@ fn main() {
         preorder_traversal(tree);
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use super::traverse_upwards;
+    use tskit::prelude::*;
+
+    pub fn make_small_table_collection_two_trees() -> tskit::TableCollection {
+        // The two trees are:
+        //  0
+        // +++
+        // | |  1
+        // | | +++
+        // 2 3 4 5
+
+        //     0
+        //   +-+-+
+        //   1   |
+        // +-+-+ |
+        // 2 4 5 3
+
+        let mut tables = tskit::TableCollection::new(1000.).unwrap();
+        tables
+            .add_node(0, 2.0, tskit::PopulationId::NULL, tskit::IndividualId::NULL)
+            .unwrap();
+        tables
+            .add_node(0, 1.0, tskit::PopulationId::NULL, tskit::IndividualId::NULL)
+            .unwrap();
+        tables
+            .add_node(
+                tskit::TSK_NODE_IS_SAMPLE,
+                0.0,
+                tskit::PopulationId::NULL,
+                tskit::IndividualId::NULL,
+            )
+            .unwrap();
+        tables
+            .add_node(
+                tskit::TSK_NODE_IS_SAMPLE,
+                0.0,
+                tskit::PopulationId::NULL,
+                tskit::IndividualId::NULL,
+            )
+            .unwrap();
+        tables
+            .add_node(
+                tskit::TSK_NODE_IS_SAMPLE,
+                0.0,
+                tskit::PopulationId::NULL,
+                tskit::IndividualId::NULL,
+            )
+            .unwrap();
+        tables
+            .add_node(
+                tskit::TSK_NODE_IS_SAMPLE,
+                0.0,
+                tskit::PopulationId::NULL,
+                tskit::IndividualId::NULL,
+            )
+            .unwrap();
+        tables.add_edge(500., 1000., 0, 1).unwrap();
+        tables.add_edge(0., 500., 0, 2).unwrap();
+        tables.add_edge(0., 1000., 0, 3).unwrap();
+        tables.add_edge(500., 1000., 1, 2).unwrap();
+        tables.add_edge(0., 1000., 1, 4).unwrap();
+        tables.add_edge(0., 1000., 1, 5).unwrap();
+        tables
+            .full_sort(tskit::TableSortOptions::default())
+            .unwrap();
+        tables.build_index().unwrap();
+        tables
+    }
+
+    #[test]
+    fn test_traverse_upwards() {
+        let tables = make_small_table_collection_two_trees();
+        let ts = tables
+            .tree_sequence(tskit::TreeSequenceFlags::default())
+            .unwrap();
+
+        let mut tree_iterator = ts.tree_iterator(tskit::TreeFlags::default()).unwrap();
+
+        while let Some(tree) = tree_iterator.next() {
+            traverse_upwards(tree);
+        }
+    }
+}
