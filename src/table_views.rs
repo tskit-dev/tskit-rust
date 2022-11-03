ee0c124561ccdca77fa3223a5fe5a6ce3c7a6b4e
Kevin R. Thornton <krthornt@uci.edu>
2022-11-03 13:52:00 -0700

    yay for tests

--- src/table_views.rs
+++ src/table_views.rs
@@ -1,18 +1,29 @@
 use crate::edge_table::EdgeTable2;
 
 ///```compile_fail
-///let mut tables = crate::TableCollection::new(100.0).unwrap();
+///let mut tables = tskit::TableCollection::new(100.0).unwrap();
 ///tables.add_edge(1., 2., 0, 1).unwrap();
 ///let edge_view = tables.edges();
 ///drop(tables);
 ///assert_eq!(tables.edges().num_rows(), 1);
+///```
+///
+///```compile_fail
+///let mut tables = tskit::TableCollection::new(100.0).unwrap();
+///tables.add_edge(1., 2., 0, 1).unwrap();
+/////get the iterator
+///let mut e = tables.edges().iter();
+///// drop our tables, breaking the liftime association
+///drop(tables);
+///// fail!
+///while let(Some(x)) = e.next() {}
 ///```
 pub struct TableViews {
     pub(crate) edges: EdgeTable2,
 }
 
 impl TableViews {
-    fn edges(&self) -> &EdgeTable2 {
+    pub fn edges(&self) -> &EdgeTable2 {
         &self.edges
     }
 }
