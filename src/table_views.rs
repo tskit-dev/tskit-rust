13d0642ebe2ce093adb35ab402acc8cf27023693
Kevin R. Thornton <krthornt@uci.edu>
2022-11-03 13:38:13 -0700

    lifetime test as doc test

--- src/table_views.rs
+++ src/table_views.rs
@@ -1,5 +1,12 @@
 use crate::edge_table::EdgeTable2;
 
+///```compile_fail
+///let mut tables = crate::TableCollection::new(100.0).unwrap();
+///tables.add_edge(1., 2., 0, 1).unwrap();
+///let edge_view = tables.edges();
+///drop(tables);
+///assert_eq!(tables.edges().num_rows(), 1);
+///```
 pub struct TableViews {
     pub(crate) edges: EdgeTable2,
 }
