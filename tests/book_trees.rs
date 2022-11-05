3a433267ad60df4719b7262a7221c6e704aac97d
Kevin R. Thornton <krthornt@uci.edu>
2022-11-05 12:53:01 -0700

    doc: book section on the tree API.

--- tests/book_trees.rs
+++ tests/book_trees.rs
@@ -73,4 +73,77 @@ fn initialize_from_table_collection() {
         // _tree is a tskit::Tree
     }
     // ANCHOR_END: iterate_trees
+
+    let mut tree_iterator = treeseq.tree_iterator(TreeFlags::default()).unwrap();
+    // ANCHOR: iterate_node_siblings
+    // This is an enum defining supported
+    // traversal orders through a Tree.
+    use tskit::NodeTraversalOrder;
+    while let Some(tree) = tree_iterator.next() {
+        for node in tree.traverse_nodes(NodeTraversalOrder::Preorder) {
+            if let Some(parent) = tree.parent(node) {
+                // Collect the siblings of node into a Vec
+                // The children function returns another iterator
+                let _siblings = if let Some(child_iterator) = tree.children(parent) {
+                    child_iterator
+                        .filter(|child| child != &node)
+                        .collect::<Vec<_>>()
+                } else {
+                    // assign empty vector
+                    vec![]
+                };
+            }
+        }
+    }
+    // ANCHOR_END: iterate_node_siblings
+
+    let mut tree_iterator = treeseq.tree_iterator(TreeFlags::default()).unwrap();
+    // ANCHOR: iterate_node_siblings_via_arrays
+    while let Some(tree) = tree_iterator.next() {
+        let parents = tree.parent_array();
+        let rsibs = tree.right_sib_array();
+        let lchildren = tree.left_child_array();
+        for node in tree.traverse_nodes(NodeTraversalOrder::Preorder) {
+            let mut siblings = vec![];
+            assert!(!node.is_null());
+            if let Some(parent) = parents.get(usize::try_from(node).unwrap()) {
+                if !parent.is_null() {
+                    if let Some(child) = lchildren.get(usize::try_from(*parent).unwrap()) {
+                        let mut u = *child;
+                        while !u.is_null() {
+                            if u != node {
+                                siblings.push(u);
+                            }
+                            if let Some(sib) = rsibs.get(usize::try_from(u).unwrap()) {
+                                u = *sib;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // ANCHOR_END: iterate_node_siblings_via_arrays
+
+    let mut tree_iterator = treeseq.tree_iterator(TreeFlags::default()).unwrap();
+    // ANCHOR: iterate_node_siblings_via_array_getters
+    while let Some(tree) = tree_iterator.next() {
+        for node in tree.traverse_nodes(NodeTraversalOrder::Preorder) {
+            let mut siblings = vec![];
+            if let Some(parent) = tree.parent(node) {
+                if let Some(child) = tree.left_child(parent) {
+                    let mut u = child;
+                    while !u.is_null() {
+                        if u != node {
+                            siblings.push(u);
+                        }
+                        if let Some(sib) = tree.right_sib(u) {
+                            u = sib;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // ANCHOR_END: iterate_node_siblings_via_array_getters
 }
