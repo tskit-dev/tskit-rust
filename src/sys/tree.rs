22391dca21b14811f39dc35522bf40b749dd1c90
Kevin R. Thornton <krthornt@uci.edu>
2023-10-11 10:20:19 -0700

    replace MBox with TskBox for low-level tree (#551)
    * remove mbox as dependency

--- src/sys/tree.rs
+++ src/sys/tree.rs
@@ -1,14 +1,11 @@
-use std::ptr::NonNull;
-
-use mbox::MBox;
-
 use super::bindings::tsk_tree_t;
 use super::flags::TreeFlags;
+use super::tskbox::TskBox;
 use super::Error;
 use super::LLTreeSeq;
 
 pub struct LLTree<'treeseq> {
-    inner: MBox<tsk_tree_t>,
+    inner: TskBox<tsk_tree_t>,
     // NOTE: this reference exists becaust tsk_tree_t
     // contains a NON-OWNING pointer to tsk_treeseq_t.
     // Thus, we could theoretically cause UB without
@@ -19,28 +16,16 @@ pub struct LLTree<'treeseq> {
 
 impl<'treeseq> LLTree<'treeseq> {
     pub fn new(treeseq: &'treeseq LLTreeSeq, flags: TreeFlags) -> Result<Self, Error> {
-        // SAFETY: this is the type we want :)
-        let temp = unsafe {
-            libc::malloc(std::mem::size_of::<super::bindings::tsk_tree_t>())
-                as *mut super::bindings::tsk_tree_t
-        };
-
-        // Get our pointer into MBox ASAP
-        let nonnull = NonNull::<super::bindings::tsk_tree_t>::new(temp)
-            .ok_or_else(|| Error::Message("failed to malloc tsk_tree_t".to_string()))?;
-
-        // SAFETY: if temp is NULL, we have returned Err already.
-        let mut inner = unsafe { mbox::MBox::from_non_null_raw(nonnull) };
-        let mut rv = unsafe {
-            super::bindings::tsk_tree_init(inner.as_mut(), treeseq.as_ptr(), flags.bits())
-        };
-        if rv < 0 {
-            return Err(Error::Code(rv));
-        }
+        let mut inner = TskBox::new(
+            |x: *mut super::bindings::tsk_tree_t| unsafe {
+                super::bindings::tsk_tree_init(x, treeseq.as_ref(), flags.bits())
+            },
+            super::bindings::tsk_tree_free,
+        );
         // Gotta ask Jerome about this one--why isn't this handled in tsk_tree_init??
         if !flags.contains(TreeFlags::NO_SAMPLE_COUNTS) {
             // SAFETY: nobody is null here.
-            rv = unsafe {
+            let rv = unsafe {
                 super::bindings::tsk_tree_set_tracked_samples(
                     inner.as_mut(),
                     treeseq.num_samples(),
@@ -55,14 +40,6 @@ impl<'treeseq> LLTree<'treeseq> {
     }
 
     pub fn as_mut_ptr(&mut self) -> *mut tsk_tree_t {
-        MBox::<tsk_tree_t>::as_mut_ptr(&mut self.inner)
-    }
-}
-
-impl<'treeseq> Drop for LLTree<'treeseq> {
-    fn drop(&mut self) {
-        // SAFETY: Mbox<_> cannot hold a NULL ptr
-        let rv = unsafe { super::bindings::tsk_tree_free(self.inner.as_mut()) };
-        assert_eq!(rv, 0);
+        self.inner.as_mut()
     }
 }
