5423b75801e89e28c5c45dc07ffbd5e02f4e7908
Kevin R. Thornton <krthornt@uci.edu>
2024-07-12 16:01:51 -0700

    do we need this

--- tests/test_table_traits.rs
+++ tests/test_table_traits.rs
@@ -101,6 +101,10 @@ impl IteratorOutput {
     }
 }
 
+struct TablesHolder<'tables> {
+    tables: &'tables tskit::TableCollection,
+}
+
 fn validate_output_from_tables(tables: tskit::TableCollection) {
     let tables_output = IteratorOutput::new_from_tables(&tables);
     let access_output = IteratorOutput::new_from_table_access(&tables);
@@ -147,6 +151,20 @@ fn make_tables() -> tskit::TableCollection {
 fn test_traits_with_table_collection() {
     let tables = make_tables();
     validate_output_from_tables(tables)
+}
+
+#[test]
+fn test_traits_with_table_collection_holder() {
+    let tables = make_tables();
+    let tref = &tables;
+    let tables_output = IteratorOutput::new_from_tables(tref);
+    let access_output = IteratorOutput::new_from_table_access(tref);
+    assert_eq!(tables_output, access_output);
+    let iteration_output = IteratorOutput::new_from_table_iteration(tref);
+    assert_eq!(tables_output, iteration_output);
+    let boxed = Box::new(tref);
+    let dynamic_output = IteratorOutput::new_from_dyn(&boxed);
+    assert_eq!(tables_output, dynamic_output);
 }
 
 #[test]
