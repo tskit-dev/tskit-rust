a23a6beb837ddbae12625ba72c2e827e87433dd2
molpopgen <molpopgen@users.noreply.github.com>
2026-05-03 19:19:47 +0000

    deploy: 540579cdf1186bbfcc8646a87a4f31fa81626e1e

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6e4b37d8.js',
+                path_to_root + 'searchindex-06f3b587.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
