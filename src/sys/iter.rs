f6278d890aa8011d8279dfde9aa20311c6d14059
Kevin R. Thornton <krthornt@uci.edu>
2026-05-04 10:32:28 -0700

    refactor: SiteRef iteration uses only basic slice ops

--- src/sys/iter.rs
+++ src/sys/iter.rs
@@ -1,18 +1,24 @@
 pub struct SiteRefIterator<'ts> {
     pub sites: &'ts [super::bindings::tsk_site_t],
-    pub current: usize,
 }
 
 impl<'ts> Iterator for SiteRefIterator<'ts> {
     type Item = super::SiteRef<'ts>;
     fn next(&mut self) -> Option<Self::Item> {
-        let n = self.sites.get(self.current).map(|s| super::new_site_ref(s));
-        self.current += 1;
-        n
+        if let Some((l, r)) = self.sites.split_first() {
+            self.sites = r;
+            Some(super::new_site_ref(l))
+        } else {
+            None
+        }
     }
 
     fn nth(&mut self, n: usize) -> Option<Self::Item> {
-        self.current = n + 1;
-        self.sites.get(n).map(|s| super::new_site_ref(s))
+        self.sites = if n < self.sites.len() {
+            &self.sites[n..]
+        } else {
+            &[]
+        };
+        self.next()
     }
 }
