ddd48573bb17a695adec1b1bb0d979f501a1a5f3
molpopgen <molpopgen@users.noreply.github.com>
2026-05-05 17:42:44 +0000

    deploy: dd89cccca17fc9e4fcac800e8ea3fc8b6b1418c4

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-06f3b587.js',
+                path_to_root + 'searchindex-c71cb33a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
